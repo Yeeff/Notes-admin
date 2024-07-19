6ef78bc4a4a29b6cfebcd2fe1265858b4d29aa45
Yeeff <yefrygf@gmail.com>
2024-07-19 12:15:59 -0500

    feat: Add initial configuration for security authorization and authentication
    - Integrated basic security setup
    - Configured authentication mechanisms

---
+++ backend/src/main/java/com/dev2prod/demo/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.dev2prod.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(auth ->{
+                    auth.requestMatchers("/api/login").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .build();
+    }
+}
